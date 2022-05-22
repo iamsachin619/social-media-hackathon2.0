d4c4293544f63dc18137d9dc7f71ac682191d530
Sachin S <singh.sacin2702@gmail.com>
2022-05-22 11:47:28 +0530

    Routing and Nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,22 @@
-const Nav =()=>{
+const Nav =({user})=>{
  
     return(
         <div>
-            Nav
+            <nav class="navbar navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">
+                    <img src="/img/logo.png" height="50"  class="d-inline-block align-top mx-3" alt=""/>
+                    
+                </a>
+                {
+                    user?
+                    (<h4 className="text-light mx-3">
+                        Welcome, User!
+                    </h4>):   
+                     (<h4 className="text-light mx-3">
+                        Welcome, Guest!
+                    </h4>)
+                }
+            </nav>
         </div>
     )
 }
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import Nav from '../Components/Nav'
 const Dashboard =()=>{
- 
+    const navigate = useNavigate()
+
     return(
         <div>
+            <Nav/>
             Dashboard
         </div>
     )
