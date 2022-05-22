b7c7d59f110cfe282235cd806b6f6b8bbee262cd
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-05-22 14:21:43 +0530

    WIP: login screen

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -10,13 +10,13 @@ const Login = props => {
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    <input type="text" class="form-control form-control-sm" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
                 </div>
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-key" aria-hidden="true"></i></span>
                     </div>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <input type="password" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
                 <button type="submit" className="btn btn-primary pull-center">Login</button>
             </form>
