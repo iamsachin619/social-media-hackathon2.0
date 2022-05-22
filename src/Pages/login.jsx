5e9177334b580200f0caed64948626129e3d6b16
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-05-22 13:53:54 +0530

    WIP: Login component added

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,8 +1,26 @@
-const Login =()=>{
+import './Login.css';
+
+const Login = props => {
  
     return(
-        <div>
-            Login
+        <div className='card'>
+            <h3 className="App">Sign in to Post</h3>
+            <form>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">@</span>
+                    </div>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <button type="submit" className="btn btn-primary pull-center">Login</button>
+            </form>
+            <span className='App'>Don't have an account? Sign up</span>
         </div>
     )
 }
