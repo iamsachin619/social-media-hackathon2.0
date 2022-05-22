b2a74e46a744ddf11150828d732b7d33006ffa15
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-05-22 18:56:41 +0530

    WIP: signup functionality

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -7,15 +7,26 @@ const SignUp = props => {
     const [username, setUsername] = useState('')
     const [pwd, setPwd] = useState('')
     const [email, setEmail] = useState('')
-    const navigate = useNavigate()
-
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState();
+    const signUpHandler = (e) => {
+        e.preventDefault();
+        let userData = {
+            userName: username,
+            password: pwd,
+            email: email
+        }
+        setFormData(userData);
+        console.log(formData)
+    }
+ 
     return(
         <div className='card center'>
             <h3 className="App">Join Skittles today</h3>
             <form>
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+                        <span class="input-group-text" id="basic-addon1"><i className="fa fa-user" aria-hidden="true"></i></span>
                     </div>
                     <input type="text" class="form-control form-control-sm" required placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" 
                     onChange={(e)=>{
@@ -24,7 +35,7 @@ const SignUp = props => {
                 </div>
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-key" aria-hidden="true"></i></span>
+                        <span class="input-group-text" id="basic-addon1"><i className="fa fa-key" aria-hidden="true"></i></span>
                     </div>
                     <input type="password" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Password" required
                     onChange={(e)=>{
@@ -34,7 +45,7 @@ const SignUp = props => {
                 </div>
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                        <span class="input-group-text" id="basic-addon1"><i className="fa fa-envelope" aria-hidden="true"></i></span>
                     </div>
                     <input type="email" class="form-control form-control-sm" id="exampleInputEmail1" placeholder="Email" required
                     onChange={(e)=>{
@@ -43,10 +54,7 @@ const SignUp = props => {
                     />
                 </div>
                 <div className='App'>
-                    <button type="submit" className="btn btn-primary" onClick={(e)=>{                    
-                        e.preventDefault()
-                        console.log(userLoginData)           
-                    }}>Sign up</button>
+                    <button type="submit" className="btn btn-primary" onClick={signUpHandler}>Sign up</button>
                 </div>
             </form>
         </div>
